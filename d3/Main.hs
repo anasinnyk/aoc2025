6dc9d99cd1ba9387d80316735d5ed34a4bce93fb
Andrii Nasinnyk <anasinnyk@macpaw.com>
2025-12-04 11:48:36 +0200

    day3#2

--- d3/Main.hs
+++ d3/Main.hs
@@ -1,4 +1,4 @@
-import Data.Char (digitToInt)
+import Data.Char (digitToInt, intToDigit)
 
 main :: IO ()
 main = do
@@ -25,3 +25,24 @@ solve f = do
   inp <- input f
   putStrLn $ show $ sum $ map ((\(x,y) -> x * 10 + y) . findMaxAndSecondMax (0,0)) $ parse inp
 
+size = 12
+
+findLastMax :: [Int] -> (Int, [Int], [Int]) -> (Int, [Int], [Int])
+findLastMax []          r = r
+findLastMax (x:xs) (y,ys,yss) = findLastMax xs (if x >= y then (x, yss ++ (y:ys), []) else (y, ys, x:yss))
+
+findXLastMaxs :: Int -> [Int] -> [Int]
+findXLastMaxs 0 r  = []
+findXLastMaxs x xs | x >= length xs = xs
+                   | otherwise = let
+    rxs = reverse xs
+    checkMaxOn = drop (x-1) rxs
+    protectTail = reverse $ take (x-1) rxs
+    (i, j, _) = findLastMax (tail checkMaxOn) (head checkMaxOn, [], [])
+  in
+    i:findXLastMaxs (x-1) (j ++ protectTail)
+
+solve' :: FilePath -> IO ()
+solve' f = do
+  inp <- input f
+  putStrLn $ show $ sum $ map (read . map intToDigit . findXLastMaxs size) $ parse inp
