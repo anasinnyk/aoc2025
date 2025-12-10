90d36f8ee9958201bb2570df8ec20bacf0f884d9
Andrii Nasinnyk <anasinnyk@macpaw.com>
2025-12-10 22:41:34 +0200

    day4#1

--- d3/Main.hs
+++ d3/Main.hs
@@ -11,38 +11,43 @@ parse :: String -> [[Int]]
 parse f = map (map digitToInt) $ lines f
 
 findMaxAndSecondMax :: (Int, Int) -> [Int] -> (Int, Int)
-findMaxAndSecondMax r      []     = r
-findMaxAndSecondMax (m,sm) (x:[]) = (m, if x > sm then x else sm)
-findMaxAndSecondMax (m,sm) (s:ss) = (if s > m
-                                    then findMaxAndSecondMax (s, 0)
-                                    else if s > sm
-                                    then findMaxAndSecondMax (m, s)
-                                    else findMaxAndSecondMax (m, sm))
-                                    ss
+findMaxAndSecondMax r [] = r
+findMaxAndSecondMax (m, sm) [x] = (m, max x sm)
+findMaxAndSecondMax (m, sm) (s : ss) =
+  ( if s > m
+      then findMaxAndSecondMax (s, 0)
+      else
+        if s > sm
+          then findMaxAndSecondMax (m, s)
+          else findMaxAndSecondMax (m, sm)
+  )
+    ss
 
 solve :: FilePath -> IO ()
 solve f = do
   inp <- input f
-  putStrLn $ show $ sum $ map ((\(x,y) -> x * 10 + y) . findMaxAndSecondMax (0,0)) $ parse inp
+  print $ sum $ map ((\(x, y) -> x * 10 + y) . findMaxAndSecondMax (0, 0)) $ parse inp
 
 size = 12
 
 findLastMax :: [Int] -> (Int, [Int], [Int]) -> (Int, [Int], [Int])
-findLastMax []          r = r
-findLastMax (x:xs) (y,ys,yss) = findLastMax xs (if x >= y then (x, yss ++ (y:ys), []) else (y, ys, x:yss))
+findLastMax [] r = r
+findLastMax (x : xs) (y, ys, yss) = findLastMax xs (if x >= y then (x, yss ++ (y : ys), []) else (y, ys, x : yss))
 
 findXLastMaxs :: Int -> [Int] -> [Int]
-findXLastMaxs 0 r  = []
-findXLastMaxs x xs | x >= length xs = xs
-                   | otherwise = let
-    rxs = reverse xs
-    checkMaxOn = drop (x-1) rxs
-    protectTail = reverse $ take (x-1) rxs
-    (i, j, _) = findLastMax (tail checkMaxOn) (head checkMaxOn, [], [])
-  in
-    i:findXLastMaxs (x-1) (j ++ protectTail)
+findXLastMaxs 0 r = []
+findXLastMaxs x xs
+  | x >= length xs = xs
+  | otherwise =
+      let
+        rxs = reverse xs
+        checkMaxOn = drop (x - 1) rxs
+        protectTail = reverse $ take (x - 1) rxs
+        (i, j, _) = findLastMax (tail checkMaxOn) (head checkMaxOn, [], [])
+       in
+        i : findXLastMaxs (x - 1) (j ++ protectTail)
 
 solve' :: FilePath -> IO ()
 solve' f = do
   inp <- input f
-  putStrLn $ show $ sum $ map (read . map intToDigit . findXLastMaxs size) $ parse inp
+  print $ sum $ map (read . map intToDigit . findXLastMaxs size) $ parse inp
---
+++ d4/Main.hs
@@ -0,0 +1,61 @@
+{-# LANGUAGE TupleSections #-}
+
+import Data.Map as M (Map, filter, fromList, insert, size, (!))
+
+main :: IO ()
+main = do
+  putStrLn "AOC 2025 #4"
+
+input :: FilePath -> IO String
+input = readFile
+
+parse :: String -> Map Int (Map Int Char)
+parse s = fromList . zip [1 ..] $ map (fromList . zip [1 ..]) $ lines s
+
+emptyRow :: Int -> Map Int Char
+emptyRow s = fromList $ take s $ map (,emptyElement) [1 ..]
+
+emptyElement :: Char
+emptyElement = '.'
+
+addEmptySquer :: Map Int (Map Int Char) -> Map Int (Map Int Char)
+addEmptySquer m =
+  let
+    withZeroRow m = insert 0 (emptyRow $ size $ m ! 1) m
+    withLatestRow m = insert (size m) (emptyRow $ size $ m ! 1) m
+    withZeroCol = fmap (insert 0 emptyElement)
+    withLatestCol m = fmap (insert (size $ m ! 1) emptyElement) m
+   in
+    withLatestCol $ withZeroCol $ withLatestRow $ withZeroRow m
+
+updateWithX :: Map Int (Map Int Char) -> Map Int (Map Int Char)
+updateWithX sm = updateWithXBy (cSize - 1) (rSize - 1) sm
+ where
+  cSize = size sm
+  rSize = size $ sm ! 1
+
+updateWithXBy :: Int -> Int -> Map Int (Map Int Char) -> Map Int (Map Int Char)
+updateWithXBy 0 0 m = m
+updateWithXBy x 0 m = updateWithXBy (x - 1) (size (m ! 1) - 1) m
+updateWithXBy x y m | m ! x ! y /= '@' = updateWithXBy x (y - 1) m
+updateWithXBy x y m =
+  let
+    rollNeigbors =
+      [ m ! (x - 1) ! (y - 1)
+      , m ! x ! (y - 1)
+      , m ! (x + 1) ! (y - 1)
+      , m ! (x - 1) ! y
+      , m ! x ! y
+      , m ! (x + 1) ! y
+      , m ! (x - 1) ! (y + 1)
+      , m ! x ! (y + 1)
+      , m ! (x + 1) ! (y + 1)
+      ]
+    rollsWithNeigborsSize = length $ Prelude.filter (/= emptyElement) rollNeigbors
+   in
+    updateWithXBy x (y - 1) $ if 5 <= rollsWithNeigborsSize then m else insert x (insert y 'X' $ m ! x) m
+
+solve :: FilePath -> IO ()
+solve f = do
+  inp <- input f
+  print $ foldr (\m acc -> acc + size (M.filter (== 'X') m)) 0 $ updateWithX $ addEmptySquer $ parse inp
