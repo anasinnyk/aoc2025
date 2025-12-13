df7592c55c45c152e870ad705a1e0d441da91400
Andrii Nasinnyk <anasinnyk@macpaw.com>
2025-12-13 02:54:28 +0200

    day8#2

--- d8/Main.hs
+++ d8/Main.hs
@@ -15,6 +15,9 @@ input = readFile
 
 type Point = (Double, Double, Double)
 
+getX :: Point -> Double
+getX (x, _, _) = x
+
 parse :: String -> [Point]
 parse s = map ((\(x : y : z : _) -> (x, y, z)) . map (read . unpack) . splitOn "," . pack) $ lines s
 
@@ -25,9 +28,14 @@ solve f = do
   let arr = array (0, length ps - 1) $ zip [0 ..] ps
   let arr' = array (0, length ps - 1) $ map (,[]) [0 .. length ps - 1]
   let dists = filter (\(_, _, r) -> r > 0) $ sortBy (\(_, _, d) (_, _, d') -> compare d d') $ concat $ createMatrixWithDistance arr
-  let res = take 1000 $ map fst $ filter (odd . snd) $ zip dists [1 ..]
+  let res = map fst $ filter (odd . snd) $ zip dists [1 ..]
+  let res10 = take 10 res
+  let res1000 = take 1000 res
+  let res' = fillTheList arr' res ! 0
 
-  print $ product $ map length $ take 3 $ sortBy (\x y -> length y `compare` length x) $ (\arr'' -> nub [sort (i : ns) | (i, ns) <- assocs arr'']) $ fillTheList arr' res
+  print $ product $ map length $ take 3 $ sortBy (\x y -> length y `compare` length x) $ (\arr'' -> nub [sort (i : ns) | (i, ns) <- assocs arr'']) $ fillTheList arr' res1000
+  print $ product $ map length $ take 3 $ sortBy (\x y -> length y `compare` length x) $ (\arr'' -> nub [sort (i : ns) | (i, ns) <- assocs arr'']) $ fillTheList arr' res10
+  print $ getX (arr ! head res') * getX (arr ! last res')
 
 fillTheList :: (Ix i) => Array i [i] -> [(i, i, b)] -> Array i [i]
 fillTheList arr [] = arr
